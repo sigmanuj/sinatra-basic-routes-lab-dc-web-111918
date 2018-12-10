a72585b6e4ce52ae811411cc964c9911f2a67f26
github username <github email address>
2018-12-10 12:44:43 -0500

    sinatra basic routes complete

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Anuj"
+  end
+
+  get '/hometown' do
+    "My hometown is New Orleans"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Steady Mobin"
+  end
 end
